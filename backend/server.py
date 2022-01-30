0f2278426cef7787e32c00112e7a20ff116bcb8d
azamat1ch <carabus228@gmail.com>
2022-01-30 13:01:11 +0800

    Added CompanyPage to server.py

--- backend/server.py
+++ backend/server.py
@@ -30,6 +30,27 @@ def topNcompanies(count):
 
 @app.route("/companyPage/<tickername>")
 def companyPage(tickername):
+    
+    company_info = si.get_company_info(tickername)
+    company_info.loc['ticker','Value'] = tickername
+    website_link = company_info.loc['website','Value']
+    company_info.loc['imagelink','Value'] = f'<img src="//logo.clearbit.com/{website_link}">'
+
+    quarterly_financials = si.get_income_statement(tickername, yearly=False)
+    quarterly_financials.index.name = None
+    quarterly_financials = quarterly_financials.T.reset_index()
+    quarterly_financials['endDate'] = quarterly_financials['endDate'].apply(lambda x: x.isoformat())
+    quarterly_financials = quarterly_financials.dropna(axis=1)
+    quarterly_financials = [row.to_dict() for index,row in quarterly_financials.iterrows()]
+
+    stock_price = si.get_data( tickername, interval='1mo').reset_index()
+    stock_price['index'] = stock_price['index'].apply(lambda x: x.isoformat())
+    stock_price = [row.to_dict() for index,row in stock_price.iterrows()]
+    company = {'info': company_info['Value'].to_dict(), 'quarterly': quarterly_financials}#, 'price':stock_price} 
+    
+    return json.dumps(company)
+    #company_info.to_json()
+#df = df.T.reset_index()
     #features = ['industry', 'revenueGrowth', 'currentPrice', 'marketCap','trailingPE', 'fiftyTwoWeekHigh', 'fiftyTwoWeekLow']
     #ticker_dict ={}
     #ticker = yf.Ticker(tickername)
