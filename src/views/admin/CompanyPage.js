8537a7f65409ab2618f5016cca1e7ac9e87b6f52
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-01-31 20:02:58 +0800

    Fixed sticky element issue on mobile

--- src/views/admin/CompanyPage.js
+++ src/views/admin/CompanyPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StickyContainer, Sticky } from "react-sticky";
 import axios from "axios";
+import { isMobile } from "react-device-detect";
 
 // components
 
@@ -34,15 +35,15 @@ const apple_esg = {
 
 const test_stocks = [
   {
-    index: '2021-07',
+    index: "2021-07",
     close: 100,
   },
   {
-    index: '2021-08',
+    index: "2021-08",
     close: 110,
   },
   {
-    index: '2021-09',
+    index: "2021-09",
     close: 130,
   },
 ];
@@ -59,7 +60,7 @@ const test_quarterly = [
   {
     incomeBeforeTax: 1000000,
     netIncome: 920000,
-  }
+  },
 ];
 
 export default class CompanyPage extends React.Component {
@@ -79,7 +80,8 @@ export default class CompanyPage extends React.Component {
           stocks: data.price.slice(-36),
           quarterly: data.quarterly,
         });
-      }).catch(e => {
+      })
+      .catch((e) => {
         console.log(e);
         this.setState({
           quarterly: test_quarterly,
@@ -93,7 +95,7 @@ export default class CompanyPage extends React.Component {
   }
 
   render() {
-    return (
+    return !isMobile ? (
       <StickyContainer>
         <div className="flex flex-wrap">
           <div className="w-full lg:w-8/12 px-4">
@@ -161,8 +163,8 @@ export default class CompanyPage extends React.Component {
                 )}
               </div>
               {/* <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
-                <CardPageVisits />
-              </div> */}
+              <CardPageVisits />
+            </div> */}
             </div>
           </div>
 
@@ -179,6 +181,84 @@ export default class CompanyPage extends React.Component {
           </div>
         </div>
       </StickyContainer>
+    ) : (
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-8/12 px-4">
+          <div className="flex flex-wrap">
+            <div className="flex flex-wrap pb4 w-full xl:w-12/12 mb-12 xl:mb-0">
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-2">
+                <MyKPICard
+                  title="Total Revenue"
+                  value="83.36 Bn"
+                  date="Sep 25, 2021"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-2">
+                <MyKPICard
+                  title="Gross Profit"
+                  value="35.17 Bn"
+                  date="Sep 25, 2021"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-2">
+                <MyKPICard
+                  title="Operating Income"
+                  value="23.79 Bn"
+                  date="Sep 25, 2021"
+                />
+              </div>
+              <div className="w-full lg:w-6/12 xl:w-3/12 px-2">
+                <MyKPICard
+                  title="Net Income"
+                  value="20.55 Bn"
+                  date="Sep 25, 2021"
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap pb4 w-full xl:w-12/12 mb-12 xl:mb-0">
+              <div className="w-full xl:w-8/12 px-4">
+                <MyESGCard esg={apple_esg} />
+              </div>
+              <div className="flex flex-wrap w-full xl:w-4/12">
+                <div className="w-full xl:w-12/12 pb-4">
+                  <MyESGKPI title="ESG - Total Score" esg={apple_esg} />
+                </div>
+              </div>
+            </div>
+            <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
+              <MyActions steps={apple_esg.Steps} />
+            </div>
+            <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
+              {this.state.stocks.length ? (
+                <MyStocksCard prices={this.state.stocks} />
+              ) : (
+                <Oval
+                  heigth="100"
+                  width="100"
+                  color="grey"
+                  ariaLabel="loading"
+                />
+              )}
+            </div>
+            <div className="w-full xl:w-12/12 px-4">
+              {this.state.quarterly.length ? (
+                <MyBarChart quarterly={this.state.quarterly} />
+              ) : (
+                <></>
+              )}
+            </div>
+            {/* <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
+                <CardPageVisits />
+              </div> */}
+          </div>
+        </div>
+
+        <div className="w-full lg:w-4/12 px-4">
+          <div className="p-4">
+            <MyCardProfile />
+          </div>
+        </div>
+      </div>
     );
   }
 }
